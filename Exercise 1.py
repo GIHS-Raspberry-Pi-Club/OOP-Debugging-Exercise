335944c2549310ccf57eb873edf88cf090adf80b
Javad Hamidi <Vadeghan@users.noreply.github.com>
2017-10-17 18:29:31 +1030

    Added first exercise

---
+++ Exercise 1.py
@@ -0,0 +1,9 @@
+class Triangle(self, base, height):
+    self.base = base
+    self.height = height
+
+    def calculate_area():
+        return height * base / 2
+
+a_triangle = Triangle(20, 30)
+print(a_triangle.calculate_area())
